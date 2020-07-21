17d6728a69cc123630ed34f9799bc9e1afed2783
todd strix laptop <tcbkc111@gmail.com>
2020-07-20 22:37:29 -0500

    working on player prefab

---
+++ Assets/Scripts/Player/MouseCameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MouseCameraController : MonoBehaviour
+{
+    [Range(0f, 90f)]
+    [SerializeField]
+    float upperVerticalLimit = 60f;
+    [Range(0f, 90f)]
+    [SerializeField]
+    float lowerVerticalLimit = 60f;
+    [SerializeField]
+    float rotateSpeed = 10f;
+
+    private Transform thisTransform;
+    private Transform camTransform;
+
+    private void Awake()
+    {
+        thisTransform = transform;
+        camTransform = GetComponentInChildren<Camera>().transform;
+    }
+
+    private void Update()
+    {
+        Pitch();
+        Yaw();
+    }
+
+    // move cam up/down
+    private void Pitch()
+    {
+        float xAngle = camTransform.eulerAngles.x
+    }
+
+    // rotate gameobject
+    private void Yaw()
+    {
+
+    }
+}
--- Assets/Scripts/Player/PCLobbyController.cs
+++ Assets/Scripts/Player/PCLobbyController.cs
@@ -3,9 +3,8 @@
 public class PCLobbyController : MonoBehaviour
 {
     private Transform thisTransform;
+    [SerializeField] private Transform forwardTransform;
     [SerializeField] private float moveSpeed = 5;
-    [SerializeField] private KeyCode leftRotate = default;
-    [SerializeField] private KeyCode rightRotate = default;
 
     private void Awake()
     {
@@ -15,32 +14,29 @@ public class PCLobbyController : MonoBehaviour
     private void Update()
     {
         Move();
-        Rotate();
     }
 
     private void Move()
     {
         Vector3 movement = Vector3.zero;
 
-        movement.z += Input.GetAxis("Vertical");
-        movement.x += Input.GetAxis("Horizontal");
+        if (forwardTransform == null)
+        {
+            movement += thisTransform.right * Input.GetAxis("Horizontal");
+            movement += thisTransform.forward * Input.GetAxis("Vertical");
+        }
+        else
+        {
+            movement += Vector3.ProjectOnPlane(
+                forwardTransform.right, 
+                thisTransform.up)
+                .normalized * Input.GetAxis("Horizontal");
+            movement += Vector3.ProjectOnPlane(
+                forwardTransform.forward, 
+                thisTransform.up)
+                .normalized * Input.GetAxis("Vertical");
+        }
 
         thisTransform.Translate(movement * moveSpeed * Time.deltaTime);
-    }
-
-    private void Rotate()
-    {
-        var rotation = thisTransform.eulerAngles;
-
-        if (Input.GetKeyDown(rightRotate))
-        {
-            rotation.y += 45;
-        }
-        if (Input.GetKeyDown(leftRotate))
-        {
-            rotation.y -= 45;
-        }
-
-        thisTransform.eulerAngles = rotation;
     }
 }
