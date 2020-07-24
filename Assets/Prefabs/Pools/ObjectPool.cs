8218608156ec6d3810261b556376d8028d765c1f
todd strix laptop <tcbkc111@gmail.com>
2020-07-24 11:04:36 -0500

    create pooling scripts

---
+++ Assets/Prefabs/Pools/ObjectPool.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private GameObject poolObject = default;
+    [SerializeField] private int preloadCount = 0;
+    [SerializeField] private bool limit = false;
+    [SerializeField] private int limitCount = 0;
+    [Header("Pools Items (For Debugging Only)")]
+    public List<GameObject> active = new List<GameObject>();
+    public List<GameObject> inactive = new List<GameObject>();
+
+    private Transform thisTransform;
+
+    protected virtual void Awake()
+    {
+        thisTransform = transform;
+    }
+
+    private void Start()
+    {
+        if (poolObject == null) return;
+        PoolManager.instance.RegisterPool(poolObject, this);
+        PreloadPool();
+    }
+
+    private void PreloadPool()
+    {
+        for (int i = PoolCount; i < preloadCount; i++)
+        {
+            GameObject obj = Instantiate(poolObject);
+            obj.transform.SetParent(thisTransform);
+            obj.SetActive(false);
+            inactive.Add(obj);
+        }
+    }
+
+    private int PoolCount
+    {
+        get
+        {
+            int _total = 0;
+            _total += active.Count;
+            _total += inactive.Count;
+            return _total;
+        }
+    }
+
+    public GameObject Spawn(Vector3 position,Quaternion rotation)
+    {
+        GameObject _spawnedObject;
+        Transform _spawnedObjectTransform;
+
+        if(inactive.Count > 0)
+        {
+            _spawnedObject = inactive[0];
+            _spawnedObjectTransform = _spawnedObject.transform;
+            inactive.RemoveAt(0);
+        }
+        else
+        {
+            if(limit && active.Count >= limitCount)
+            {
+                Debug.Log("pool limit reached", this);
+                return null;
+            }
+            _spawnedObject = Instantiate(poolObject);
+            _spawnedObjectTransform = _spawnedObject.transform;
+        }
+
+        _spawnedObjectTransform.position = position;
+        _spawnedObjectTransform.rotation = rotation;
+        _spawnedObjectTransform.parent = thisTransform;
+
+        active.Add(_spawnedObject);
+        _spawnedObject.SetActive(true);
+
+        return _spawnedObject;
+    }
+
+    public virtual void Despawn(GameObject instance)
+    {
+        active.Remove(instance);
+        inactive.Add(instance);
+        instance.SetActive(false);
+    }
+}
---
+++ Assets/Prefabs/Pools/ParticlePool.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ParticlePool : ObjectPool
+{
+    [Header("Particle Pool Settings")]
+    [SerializeField] private float despawnDelay = 2.0f;
+
+    private WaitForSeconds delay;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        delay = new WaitForSeconds(despawnDelay);
+    }
+
+    public override void Despawn(GameObject instance)
+    {
+        StartCoroutine(DespawnDelay(instance));
+        
+    }
+
+    private IEnumerator DespawnDelay(GameObject instance)
+    {
+        yield return delay;
+        ActualDespawn(instance);
+    }
+
+    private void ActualDespawn(GameObject instance)
+    {
+        active.Remove(instance);
+        inactive.Add(instance);
+        instance.SetActive(false);
+    }
+}
