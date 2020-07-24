61cdf037aafc94f6e4a854a35fd7aab17d97edfd
todd strix laptop <tcbkc111@gmail.com>
2020-07-24 12:15:58 -0500

    update photon custom room and player properties code

--- Assets/Prefabs/Network/Scripts/CustomPlayerProperties.cs
+++ Assets/Prefabs/Network/Scripts/CustomPlayerProperties.cs
@@ -1,4 +1,5 @@
-﻿using Photon.Realtime;
+﻿using Photon.Pun;
+using Photon.Realtime;
 
 /// <summary>
 /// static helper class that is used to update and reset attributes stored on the Photon Player level
@@ -6,17 +7,80 @@
 
 public static class CustomPlayerProperties
 {
-    public static void UpdateProps<T>(Player player, string key, T value)
+    public const string score = "score";
+    public const string game = "game";
+    public const string team = "team";
+
+    public static int GetCurrentScore(this PhotonView player)
     {
-        ExitGames.Client.Photon.Hashtable props = player.CustomProperties;
-        if (props.ContainsKey(key)) props.Remove(key);
-        props.Add(key, value);
-        player.SetCustomProperties(props);
+        return player.Owner.GetCurrentScore();
+    }
+    public static int GetCurrentScore(this Player player)
+    {
+        return (int)player.CustomProperties[score];
     }
 
-    public static void ResetProps(Player player)
+    public static int GetGameScene(this PhotonView player)
     {
-        ExitGames.Client.Photon.Hashtable props = new ExitGames.Client.Photon.Hashtable();
-        player.SetCustomProperties(props);
+        return player.Owner.GetGameScene();
+    }
+    public static int GetGameScene(this Player player)
+    {
+        return (int)player.CustomProperties[game];
+    }
+
+    public static int GetTeam(this PhotonView player)
+    {
+        return player.Owner.GetTeam();
+    }
+    public static int GetTeam(this Player player)
+    {
+        return (int)player.CustomProperties[team];
+    }
+
+    public static void SetScore(this PhotonView player, int value)
+    {
+        player.Owner.SetScore(value);
+    }
+    public static void SetScore(this Player player, int value)
+    {
+        var _score = new ExitGames.Client.Photon.Hashtable() { { score, (byte)value } };
+        player.SetCustomProperties(_score);
+    }
+
+    public static void SetGameScene(this PhotonView player, int value)
+    {
+        player.Owner.SetGameScene(value);
+    }
+    public static void SetGameScene(this Player player, int value)
+    {
+        var _game = new ExitGames.Client.Photon.Hashtable() { { game, (byte)value } };
+        player.SetCustomProperties(_game);
+    }
+
+    public static void SetTeam(this PhotonView player, int value)
+    {
+        player.Owner.SetTeam(value);
+    }
+    public static void SetTeam(this Player player, int value)
+    {
+        var _team = new ExitGames.Client.Photon.Hashtable() { { team, (byte)value } };
+        player.SetCustomProperties(_team);
+    }
+
+    public static void ResetProps(this PhotonView player)
+    {
+        player.Owner.ResetProps();
+    }
+
+    public static void ResetProps(this Player player)
+    {
+        var _props = new ExitGames.Client.Photon.Hashtable()
+        {
+            { score, (byte)0 },
+            { team,  (byte)0 },
+            { game,  (byte)0 }
+        };
+        player.SetCustomProperties(_props);
     }
 }
---
+++ Assets/Prefabs/Network/Scripts/CustomRoomProperties.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Realtime;
+
+public static class CustomRoomProperties
+{
+    public const string teams = "teams";
+    public const string scores = "scores";
+    public const string game = "game";
+
+    public static int[] GetTeams(this Room room)
+    {
+        return (int[])room.CustomProperties[teams];
+    }
+
+    public static int[] GetScores(this Room room)
+    {
+        return (int[])room.CustomProperties[scores];
+    }
+
+    public static int GetGameScene(this Room room)
+    {
+        return (int)room.CustomProperties[game];
+    }
+
+    public static int[] AddToTeam(this Room room, int teamIndex, int value)
+    {
+        int[] _teams = room.GetTeams();
+        _teams[teamIndex] += value;
+
+        room.SetCustomProperties(new ExitGames.Client.Photon.Hashtable() { { teams, _teams } });
+        return _teams;
+    }
+
+    public static int[] AddScore(this Room room, int teamIndex, int value)
+    {
+        int[] _scores = room.GetScores();
+        _scores[teamIndex] += value;
+
+        room.SetCustomProperties(new ExitGames.Client.Photon.Hashtable() { { scores, _scores } });
+        return _scores;
+    }
+
+    public static void SetGameScene(this Room room, int value)
+    {
+        room.SetCustomProperties(new ExitGames.Client.Photon.Hashtable() { { game, value } });
+    }
+}
