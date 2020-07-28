7281bb76e0ecb8bab579809ec20ed96bd43d334f
tcbkc111@gmail.com <tbelcher@gill-line.com>
2020-07-28 15:39:16 -0500

    create scene details scriptableobjects to programatically create the buttons for loading games

--- Assets/Prefabs/UI/Scripts/LobbyUI.cs
+++ Assets/Prefabs/UI/Scripts/LobbyUI.cs
@@ -1,14 +1,23 @@
 ﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// this ui panel allows the host to select a game scene to load
 /// </summary>
 
-public class LobbyUI : MonoBehaviour
+public class LobbyUI : MonoBehaviourPunCallbacks
 {
     [SerializeField] private GameObject panel = default;
     [SerializeField] private ScriptableObjectArchitecture.GameEvent hostSelectGameEvent = default;
+    [SerializeField] private Transform buttonPanel = default;
+    [SerializeField] private GameObject loadGameButton = default;
+    [SerializeField] private List<SceneDetails> gameScenes = new List<SceneDetails>();
+
 
     private void Start()
     {
@@ -16,6 +25,50 @@ public class LobbyUI : MonoBehaviour
         {
             panel.SetActive(false);
         }
+        else
+        {
+            UpdateGameButtons();
+        }
+    }
+
+    private void UpdateGameButtons()
+    {
+        // delete all buttons
+        DeleteButtons();
+        int players = PhotonNetwork.CurrentRoom.PlayerCount;
+        // for each scene detail:
+        foreach (var sc in gameScenes)
+        {
+            // if current players in room are valid for game
+            if (players >= sc.minPlayers && players <= sc.maxPlayers)
+            {
+                // create button to load game
+                var button = Instantiate(loadGameButton, buttonPanel);
+                button.GetComponentInChildren<Text>().text = sc.scene;
+                button.GetComponent<Button>().onClick.AddListener
+                    (
+                        delegate { HostSelectGame(sc.buildIndex); }
+                    );
+            }
+        }
+    }
+
+    private void DeleteButtons()
+    {
+        var buttons = transform.GetComponentsInChildren<Button>().ToArray();
+        for (int i = buttons.Length - 1; i >= 0; i--)
+        {
+            Destroy(buttons[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdateGameButtons();
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateGameButtons();
     }
 
     // used for ui buttons to load a game scene
---
+++ Assets/Prefabs/Utility/Scripts/SceneDetails.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="Scene Details")]
+public class SceneDetails : ScriptableObject
+{
+    public string scene;
+    public int buildIndex = 0;
+    public int maxPlayers = 4;
+    public int minPlayers = 1;
+}
